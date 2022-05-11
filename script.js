7b2037f3849b79b2032988e18c4464cd7ae722a2
amira-zak <83556228+amira-zak@users.noreply.github.com>
2022-05-11 07:49:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var sum;
+function sum() {
+    sum= vend+med+corp+pro;
+   }
+   
+   function brex(){
+    sum= vend+med+corp+pro;
+       var br= sum*0.3;
+       document.getElementById(br);
+
+   }
+   function stripe(){
+    sum= vend+med+corp+pro;
+       var st= sum*2.9;
+       document.getElementById(st);
+   }
+   function amex(){
+    sum= vend+med+corp+pro;
+       var am= sum*1.3;
+       document.getElementById(am);
+   }
+   function unlimited(){
+    sum= vend+med+corp+pro;
+       var un= sum*1.1;
+       document.getElementById(un);
+   }
+   function lifetime(){
+    sum= vend+med+corp+pro;
+       var lt= sum*1.2;
+       document.getElementById(lt);
+   }
